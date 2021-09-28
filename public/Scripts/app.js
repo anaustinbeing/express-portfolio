4e997eb2120826c6277cf27c0eb47fc5b79b651a
Austin Joyal <anaustinbeing@gmail.com>
2021-09-28 11:53:31 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+// IIFE - Immediately Invoked Function Expression
+(function (){
+
+    function Start() {
+        console.log("Application started..!");
+    }
+
+    window.addEventListener("load", Start);
+
+}());
